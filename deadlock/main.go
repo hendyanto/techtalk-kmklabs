9a8cf490d57f9babef40a7c492a8e5d535992183
Hendyanto <hendyanto.inbox@gmail.com>
2016-04-22 11:12:07 +0700

    remove unused channel

--- deadlock/main.go
+++ deadlock/main.go
@@ -5,9 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world!")
 	channel_start := make(chan string)
-	channel_end := make(chan string)
 
 	channel_start <- "Message"
 
-	<-channel_end
+	<-channel_start
 }
